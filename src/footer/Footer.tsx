0ac85c90262f30a54f9769ea9711185667ecc667
chi-wei-lien <wilsonwilson3124@gmail.com>
2024-02-03 17:12:47 -0500

    add icons to footer

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -1,5 +1,9 @@
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import styled from 'styled-components'
 
+import { IconList } from '../style/Icon'
 import SectionContainer from '../style/SectionContainer'
 
 const Footer = () => {
@@ -8,7 +12,20 @@ const Footer = () => {
   return (
     <div>
       <SectionContainer>
-        <TextSection>© 2021-{year} Chi-Wei Lien. All rights reserved.</TextSection>
+        <div>
+          <IconList>
+            <a href='https://github.com/chi-wei-lien' target='_blank' rel='noreferrer'>
+              <FontAwesomeIcon icon={faGithub} size='2xl' />
+            </a>
+            <a href='mailto:chiweilien3124@gmail.com' target='_blank' rel='noreferrer'>
+              <FontAwesomeIcon icon={faEnvelope} size='2xl' />
+            </a>
+            <a href='https://www.linkedin.com/in/chi-wei-lien/' target='_blank' rel='noreferrer'>
+              <FontAwesomeIcon icon={faLinkedin} size='2xl' />
+            </a>
+          </IconList>
+          <TextSection>© 2021-{year} Chi-Wei Lien. All rights reserved.</TextSection>
+        </div>
       </SectionContainer>
     </div>
   )
---
+++ src/style/Icon.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const IconList = styled.div`
+  display: flex;
+  justify-content: center;
+  & a {
+    padding: 10px 10px 0px 10px;
+    color: white;
+  }
+  & a:hover {
+    color: #c9c9c9;
+  }
+`
