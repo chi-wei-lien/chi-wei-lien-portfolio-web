c5cc1ebc9c6c292c116e5db2a039ee52724c6dd1
chi-wei-lien <wilsonwilson3124@gmail.com>
2024-02-02 19:07:49 -0500

    remove unused import and variables

--- src/about/About.tsx
+++ src/about/About.tsx
@@ -1,20 +1,8 @@
-import { useEffect, useState } from 'react'
-import ReactMarkdown from 'react-markdown'
-
 import { ImageContainer, ImageSection } from '../style/Image'
 import SectionContainer from '../style/SectionContainer'
 import { TextImageContainer, TextSection } from '../style/Text'
 
 const About = () => {
-  const [content, setContent] = useState('')
-  useEffect(() => {
-    fetch('/content/intro.md')
-      .then((res) => res.text())
-      .then((text) => {
-        setContent(text)
-      })
-  }, [])
-
   return (
     <div>
       <SectionContainer>
